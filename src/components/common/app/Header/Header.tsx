b3dc4e47cb5675caea76660dd73e926f10f27467
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-07-21 20:02:56 +0200

    feat: beta caption

--- src/components/common/app/Header/Header.tsx
+++ src/components/common/app/Header/Header.tsx
@@ -25,12 +25,13 @@ export default function Header() {
       <Box className={classes.placeholder} />
       <header className={classes.header}>
         <Container size="lg" className={classes.inner}>
-          <Group gap="sm" visibleFrom="xs">
+          <Group gap="xs" visibleFrom="xs">
             <Text size="lg" fw={500}>
               {t("title")}
             </Text>
+            <Text size="md">{t('betaCaption')}</Text>
           </Group>
-          <Group gap="lg" visibleFrom="xs" wrap="nowrap" align="center">
+          <Group gap="lg" visibleFrom="xs" wrap="nowrap" >
             <NavLink component={Link} href="/test" label={t("createEvent")} />
             <LocalePicker />
             <ActionIcon variant="transparent" size="md">
