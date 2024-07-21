fa62922c0181f481ecce61c4e018bd5cdd42b0f6
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-07-21 19:24:27 +0200

    feat: adjust icon

--- src/components/common/app/Header/Header.tsx
+++ src/components/common/app/Header/Header.tsx
@@ -3,7 +3,6 @@
 import { useTranslations } from "next-intl";
 import { Container, Group, Burger, Text, NavLink } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { IconTank } from "@tabler/icons-react";
 import { Link } from "@/navigation";
 import { LocalePicker } from "@components/common/app";
 import classes from "./Header.module.css";
@@ -16,8 +15,7 @@ export default function Header() {
     <header className={classes.header}>
       <Container size="md" className={classes.inner}>
         <Group gap="sm" visibleFrom="xs">
-          <IconTank size={28} />
-          <Text size="md">{t("title")}</Text>
+          <Text size="lg" fw={500}>{t("title")}</Text>
         </Group>
         <Group gap="lg" visibleFrom="xs" wrap="nowrap" align="center">
           <NavLink component={Link} href="/test" label={t("createEvent")} />
