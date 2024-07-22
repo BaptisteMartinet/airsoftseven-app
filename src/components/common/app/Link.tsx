f01dabbf9a81b872156417694ef2450e786d340e
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-07-22 22:55:31 +0200

    feat: update link

--- src/components/common/app/Link.tsx
+++ src/components/common/app/Link.tsx
@@ -1,7 +1,8 @@
-'use client'
+"use client";
 
 import type { NavLinkProps } from "@mantine/core";
 
+import React from "react";
 import { NavLink } from "@mantine/core";
 import { Link as NavigationLink } from "@/navigation";
 
@@ -9,6 +10,8 @@ export interface LinkProps extends Omit<NavLinkProps, "href" | "component"> {
   href: string;
 }
 
-export default function Link(props: LinkProps) {
-  return <NavLink component={NavigationLink} {...props} />;
-}
+const Link = React.forwardRef(function Link_(props: LinkProps, ref: any) {
+  return <NavLink ref={ref} component={NavigationLink} {...props} />;
+});
+
+export default Link;
