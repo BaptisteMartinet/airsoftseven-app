e18ec97ad3aec909a235cdbce91424fe1405a1f5
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-07-23 23:57:10 +0200

    feat(core/utils): implement handlePromiseWithToast

---
+++ src/core/utils/promise.ts
@@ -0,0 +1,27 @@
+import { showNotification } from "@mantine/notifications";
+
+interface HandlePromiseWithToastOpts<T> {
+  successMessage: string | null;
+  onSuccess?: (value: T) => void;
+  errorMessage: string;
+  onError?: (error: unknown) => void;
+}
+
+export function handlePromiseWithToast<T>(
+  promise: Promise<T>,
+  opts: HandlePromiseWithToastOpts<T>
+) {
+  const { successMessage, onSuccess, errorMessage, onError } = opts;
+  return promise
+    .then((value) => {
+      if (successMessage)
+        showNotification({ message: successMessage, color: "green" });
+      if (onSuccess) onSuccess(value);
+      return value;
+    })
+    .catch((error) => {
+      if (errorMessage)
+        showNotification({ message: errorMessage, color: "red" });
+      if (onError) onError(error);
+    });
+}
