65f9c15de03ad87d3aeb317b7207852cdb222bc5
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-07-23 23:33:19 +0200

    feat: setup notifications

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,7 +5,9 @@ import { getMessages } from "next-intl/server";
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
 import "@mantine/carousel/styles.css";
+import "@mantine/notifications/styles.css";
 import { createTheme, ColorSchemeScript, MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
 import { ApolloProvider } from "@providers/index";
 
 const theme = createTheme({});
@@ -36,7 +38,10 @@ export default async function LocaleLayout(
       <body>
         <ApolloProvider>
           <NextIntlClientProvider messages={messages}>
-            <MantineProvider theme={theme}>{children}</MantineProvider>
+            <MantineProvider theme={theme}>
+              <Notifications />
+              {children}
+            </MantineProvider>
           </NextIntlClientProvider>
         </ApolloProvider>
       </body>
