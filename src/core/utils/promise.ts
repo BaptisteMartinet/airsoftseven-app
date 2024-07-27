fd4a88e95abce90655999cf88461293ed0fbfc9b
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-07-27 12:18:45 +0200

    feat(core/utils): implement usePromiseStatusWithToast

--- src/core/utils/promise.ts
+++ src/core/utils/promise.ts
@@ -1,5 +1,7 @@
 import { showNotification } from "@mantine/notifications";
 
+import React from "react";
+
 interface HandlePromiseWithToastOpts<T> {
   successMessage?: string;
   onSuccess?: (value: T) => void;
@@ -24,4 +26,34 @@ export function handlePromiseWithToast<T>(
         showNotification({ message: errorMessage, color: "red" });
       if (onError) onError(error);
     });
+}
+
+export type PromiseStatus =
+  | "uninitialized"
+  | "pending"
+  | "rejected"
+  | "fulfilled";
+
+export function usePromiseStatusWithToast() {
+  const [status, setStatus] = React.useState<PromiseStatus>("uninitialized");
+
+  function handlePromise<T>(
+    promise: Promise<T>,
+    args: HandlePromiseWithToastOpts<T>
+  ) {
+    const { onSuccess, onError, ...rest } = args;
+    setStatus("pending");
+    handlePromiseWithToast(promise, {
+      onSuccess: (val) => {
+        setStatus("fulfilled");
+        onSuccess?.(val);
+      },
+      onError: (err) => {
+        setStatus("rejected");
+        onError?.(err);
+      },
+      ...rest,
+    });
+  }
+  return [status, handlePromise] as const;
 }
