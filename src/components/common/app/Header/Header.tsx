baa35080cf2bd895096c3a0ef8a834182f0b63f5
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-07-27 11:35:42 +0200

    refactor(components/common): use common Link component

--- src/components/common/app/Header/Header.tsx
+++ src/components/common/app/Header/Header.tsx
@@ -1,17 +1,9 @@
 "use client";
 
 import { useTranslations } from "next-intl";
-import {
-  Container,
-  Group,
-  Burger,
-  Text,
-  NavLink,
-  Box,
-} from "@mantine/core";
+import { Container, Group, Burger, Text, Box } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { Link } from "@/navigation";
-import { LocalePicker, Anchor } from "@components/common";
+import { LocalePicker, Anchor, Link } from "@components/common";
 import classes from "./Header.module.css";
 import ProfileMenu from "./ProfileMenu";
 
@@ -33,11 +25,7 @@ export default function Header() {
             </Anchor>
           </Group>
           <Group gap="lg" visibleFrom="xs" wrap="nowrap">
-            <NavLink
-              component={Link}
-              href="/event/create"
-              label={t("createEvent")}
-            />
+            <Link href="/event/create" label={t("createEvent")} />
             <LocalePicker />
             <ProfileMenu />
           </Group>
