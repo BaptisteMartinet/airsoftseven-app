c225b067664b9becd551cb146e4efb3c28491aab
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-07-30 22:54:38 +0200

    feat(providers): fetch session on on initial render

--- src/providers/SessionProvider.tsx
+++ src/providers/SessionProvider.tsx
@@ -1,10 +1,35 @@
 "use client";
 
 import type { ReactNode } from "react";
+import type { TypedDocumentNode } from "@apollo/client";
+import type { IdType } from "@/core/api/types";
 
 import React from "react";
 import { useStore } from "zustand";
+import { gql, useApolloClient } from "@apollo/client";
 import { createSessionStore, type SessionStore } from "@stores/session";
+
+const SessionQuery: TypedDocumentNode<{
+  session: {
+    id: IdType;
+    token: string;
+    user: {
+      id: IdType;
+      username: string;
+    };
+  };
+}> = gql`
+  query GetSession {
+    session {
+      id
+      token
+      user {
+        id
+        username
+      }
+    }
+  }
+`;
 
 type SessionStoreApi = ReturnType<typeof createSessionStore>;
 
@@ -16,8 +41,19 @@ interface SessionStoreProviderProps {
 
 export default function SessionStoreProvider(props: SessionStoreProviderProps) {
   const { children } = props;
+  const client = useApolloClient();
+
   const storeRef = React.useRef<SessionStoreApi>();
   if (!storeRef.current) storeRef.current = createSessionStore();
+
+  React.useEffect(() => {
+    async function initSession() {
+      const session = await client.query({ query: SessionQuery });
+      storeRef.current?.setState({ session: session.data.session }, true);
+    }
+    initSession().catch(console.error);
+  }, [client]);
+
   return (
     <SessionStoreContext.Provider value={storeRef.current}>
       {children}
