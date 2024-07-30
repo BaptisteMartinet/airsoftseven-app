50e640198e896eb4be8ff369e412345abeb68f62
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-07-30 21:10:59 +0200

    feat: imrpove mantine provider

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,12 +2,8 @@ import type { Metadata } from "next";
 
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages, getTranslations } from "next-intl/server";
-import "@mantine/core/styles.css";
-import "@mantine/dates/styles.css";
-import "@mantine/carousel/styles.css";
-import "@mantine/notifications/styles.css";
+
 import { ColorSchemeScript } from "@mantine/core";
-import { Notifications } from "@mantine/notifications";
 import Providers from "@providers/Providers";
 
 export async function generateMetadata({
@@ -33,10 +29,7 @@ export default async function LocaleLayout(
       </head>
       <body>
         <NextIntlClientProvider messages={messages}>
-          <Providers>
-            <Notifications />
-            {children}
-          </Providers>
+          <Providers>{children}</Providers>
         </NextIntlClientProvider>
       </body>
     </html>
--- src/providers/MantineProvider.tsx
+++ src/providers/MantineProvider.tsx
@@ -1,10 +1,20 @@
 import type { PropsWithChildren } from "react";
 
+import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
+import "@mantine/carousel/styles.css";
+import "@mantine/notifications/styles.css";
 import { MantineProvider as MantineWrapper, createTheme } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
 
 const theme = createTheme({});
 
 export default function MantineProvider(props: PropsWithChildren) {
   const { children } = props;
-  return <MantineWrapper theme={theme}>{children}</MantineWrapper>;
+  return (
+    <MantineWrapper theme={theme}>
+      {children}
+      <Notifications />
+    </MantineWrapper>
+  );
 }
