13bd8cd529204accc937e9abc94ac443b21a4619
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-07-30 22:07:11 +0200

    feat: implement profil menu item

--- src/components/common/app/Header/ProfileMenu/ProfileMenu.tsx
+++ src/components/common/app/Header/ProfileMenu/ProfileMenu.tsx
@@ -2,9 +2,11 @@ import { useTranslations } from "next-intl";
 import { ActionIcon, Menu, Divider } from "@mantine/core";
 import { IconUserCircle } from "@tabler/icons-react";
 import { Link } from "@/navigation";
+import { useSessionStore } from "@/providers";
 
 export default function ProfileMenu() {
   const t = useTranslations("common.Header.ProfileMenu");
+  const session = useSessionStore((state) => state.session);
   return (
     <Menu shadow="md">
       <Menu.Target>
@@ -14,12 +16,20 @@ export default function ProfileMenu() {
       </Menu.Target>
 
       <Menu.Dropdown>
-        <Menu.Item component={Link} href="/login">
-          {t("login")}
-        </Menu.Item>
-        <Menu.Item component={Link} href="/register">
-          {t("register")}
-        </Menu.Item>
+        {session ? (
+          <Menu.Item component={Link} href="/profile">
+            {t("profile")}
+          </Menu.Item>
+        ) : (
+          <>
+            <Menu.Item component={Link} href="/login">
+              {t("login")}
+            </Menu.Item>
+            <Menu.Item component={Link} href="/register">
+              {t("register")}
+            </Menu.Item>
+          </>
+        )}
         <Divider my="sm" />
         <Menu.Item component={Link} href="/help">
           {t("help")}
