fd0c1a9347a943502ef47dd71b83364f6e4ccff7
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-09-08 21:56:55 +0200

    feat(components/common): implement AddressPicker

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -3,3 +3,4 @@ export { default as PinInputModal } from "./PinInputModal";
 export { default as SearchableSelect } from "./SearchableSelect";
 export { default as AsyncSelect } from "./AsyncSelect";
 export { default as NullableNumberInput } from './NullableNumberInput';
+export { default as AddressPicker } from "./AddressPicker";
