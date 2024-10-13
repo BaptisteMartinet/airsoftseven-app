bfca8057c12cacd48f76cd4951017954c3e2dc15
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-10-13 22:58:35 +0200

    config: add experimental optimizePackageImports to enable mantine tree shaking

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode: false,
+  experimental: {
+    optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
+  },
 };
 
 export default withNextIntl(nextConfig);
