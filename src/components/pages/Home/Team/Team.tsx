75aad855cf24ddec6b8046f144a683accfad30c5
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-10-16 23:45:02 +0200

    feat: implement Team section

--- src/components/pages/Home/Team/Team.tsx
+++ src/components/pages/Home/Team/Team.tsx
@@ -1,25 +1,29 @@
 "use client";
 
-import { Carousel } from "@mantine/carousel";
-import { useMediaQuery } from "@mantine/hooks";
+import React from "react";
+import { useTranslations } from "next-intl";
 import {
-  Paper,
-  Text,
-  Title,
-  Button,
   useMantineTheme,
   rem,
   Container,
+  Paper,
+  Text,
+  Title,
 } from "@mantine/core";
+import { Carousel } from "@mantine/carousel";
+import { useMediaQuery } from "@mantine/hooks";
 import classes from "./Team.module.css";
 
-interface CardProps {
+interface TeamMember {
   image: string;
-  title: string;
-  category: string;
+  username: string;
+  role: string;
 }
 
-function Card({ image, title, category }: CardProps) {
+interface CardProps extends TeamMember {}
+
+function Card(props: CardProps) {
+  const { image, username, role } = props;
   return (
     <Paper
       shadow="md"
@@ -30,74 +34,64 @@ function Card({ image, title, category }: CardProps) {
     >
       <div>
         <Text className={classes.category} size="xs">
-          {category}
+          {role}
         </Text>
         <Title order={3} className={classes.title}>
-          {title}
+          {username}
         </Title>
       </div>
-      <Button variant="white" color="dark">
-        Read article
-      </Button>
     </Paper>
   );
 }
 
-const data = [
-  {
-    image:
-      "https://images.unsplash.com/photo-1508193638397-1c4234db14d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-    title: "Best forests to visit in North America",
-    category: "nature",
-  },
-  {
-    image:
-      "https://images.unsplash.com/photo-1559494007-9f5847c49d94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-    title: "Hawaii beaches review: better than you think",
-    category: "beach",
-  },
-  {
-    image:
-      "https://images.unsplash.com/photo-1608481337062-4093bf3ed404?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-    title: "Mountains at night: 12 best locations to enjoy the view",
-    category: "nature",
-  },
-  {
-    image:
-      "https://images.unsplash.com/photo-1507272931001-fc06c17e4f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-    title: "Aurora in Norway: when to visit for best experience",
-    category: "nature",
-  },
-  {
-    image:
-      "https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-    title: "Best places to visit this winter",
-    category: "tourism",
-  },
-  {
-    image:
-      "https://images.unsplash.com/photo-1582721478779-0ae163c05a60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-    title: "Active volcanos reviews: travel at your own risk",
-    category: "nature",
-  },
-];
+function useTeamMembers(): Array<TeamMember> {
+  const t = useTranslations("pages.Home.Team");
+
+  return [
+    {
+      image: "/player-placeholder-3.jpg",
+      username: t("member1Username"),
+      role: t("member1Role"),
+    },
+    {
+      image: "/player-placeholder-2.jpg",
+      username: t("member2Username"),
+      role: t("member2Role"),
+    },
+    {
+      image: "/player-placeholder-1.jpg",
+      username: t("member3Username"),
+      role: t("member3Role"),
+    },
+    {
+      image: "/player-placeholder-4.jpg",
+      username: t("member4Username"),
+      role: t("member4Role"),
+    },
+  ];
+}
 
 export default function Team() {
+  const t = useTranslations("pages.Home.Team");
   const theme = useMantineTheme();
   const mobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
-  const slides = data.map((item) => (
-    <Carousel.Slide key={item.title}>
-      <Card {...item} />
+
+  const teamMembers = useTeamMembers();
+  const slides = teamMembers.map((member) => (
+    <Carousel.Slide key={member.username}>
+      <Card {...member} />
     </Carousel.Slide>
   ));
 
   return (
     <Container>
+      <Title my="md">{t("title")}</Title>
       <Carousel
         slideSize={{ base: "100%", sm: "50%" }}
         slideGap={{ base: rem(2), sm: "xl" }}
         align="start"
         slidesToScroll={mobile ? 1 : 2}
+        withIndicators
       >
         {slides}
       </Carousel>
