503a7d32cdad415fed6c3b2526a6fdb06f035801
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-10-16 23:07:29 +0200

    feat: setup Faq texts

--- src/components/pages/Home/Faq/Faq.tsx
+++ src/components/pages/Home/Faq/Faq.tsx
@@ -1,3 +1,4 @@
+import { useTranslations } from "next-intl";
 import {
   Container,
   Title,
@@ -8,48 +9,39 @@ import {
 } from "@mantine/core";
 import classes from "./Faq.module.css";
 
-const placeholder =
-  "It can’t help but hear a pin drop from over half a mile away, so it lives deep in the mountains where there aren’t many people or Pokémon.It was born from sludge on the ocean floor. In a sterile environment, the germs within its body can’t multiply, and it dies.It has no eyeballs, so it can’t see. It checks its surroundings via the ultrasonic waves it emits from its mouth.";
+export default function Faq() {
+  const t = useTranslations("pages.Home.Faq");
 
-export default function FaqSimple() {
   return (
     <Container size="sm" className={classes.wrapper}>
       <Title ta="center" className={classes.title}>
-        Frequently Asked Questions
+        {t("title")}
       </Title>
 
       <Accordion variant="separated">
-        <AccordionItem className={classes.item} value="reset-password">
-          <AccordionControl>How can I reset my password?</AccordionControl>
-          <AccordionPanel>{placeholder}</AccordionPanel>
+        <AccordionItem className={classes.item} value="question1">
+          <AccordionControl>{t("question1Title")}</AccordionControl>
+          <AccordionPanel>{t("question1Desc")}</AccordionPanel>
         </AccordionItem>
 
-        <AccordionItem className={classes.item} value="another-account">
-          <AccordionControl>
-            Can I create more that one account?
-          </AccordionControl>
-          <AccordionPanel>{placeholder}</AccordionPanel>
+        <AccordionItem className={classes.item} value="question2">
+          <AccordionControl>{t("question2Title")}</AccordionControl>
+          <AccordionPanel>{t("question2Desc")}</AccordionPanel>
         </AccordionItem>
 
-        <AccordionItem className={classes.item} value="newsletter">
-          <AccordionControl>
-            How can I subscribe to monthly newsletter?
-          </AccordionControl>
-          <AccordionPanel>{placeholder}</AccordionPanel>
+        <AccordionItem className={classes.item} value="question3">
+          <AccordionControl>{t("question3Title")}</AccordionControl>
+          <AccordionPanel>{t("question3Desc")}</AccordionPanel>
         </AccordionItem>
 
-        <AccordionItem className={classes.item} value="credit-card">
-          <AccordionControl>
-            Do you store credit card information securely?
-          </AccordionControl>
-          <AccordionPanel>{placeholder}</AccordionPanel>
+        <AccordionItem className={classes.item} value="question4">
+          <AccordionControl>{t("question4Title")}</AccordionControl>
+          <AccordionPanel>{t("question4Desc")}</AccordionPanel>
         </AccordionItem>
 
-        <AccordionItem className={classes.item} value="payment">
-          <AccordionControl>
-            What payment systems to you work with?
-          </AccordionControl>
-          <AccordionPanel>{placeholder}</AccordionPanel>
+        <AccordionItem className={classes.item} value="question5">
+          <AccordionControl>{t("question5Title")}</AccordionControl>
+          <AccordionPanel>{t("question5Desc")}</AccordionPanel>
         </AccordionItem>
       </Accordion>
     </Container>
