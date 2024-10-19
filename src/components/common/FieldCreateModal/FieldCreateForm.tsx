45880caadee06a12e6d80a7b3079dbf8d9af1bbf
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-10-19 16:29:10 +0200

    feat(field-create-form): improve spacings

--- src/components/common/FieldCreateModal/FieldCreateForm.tsx
+++ src/components/common/FieldCreateModal/FieldCreateForm.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import assert from "assert";
 import { useTranslations } from "next-intl";
 import { useApolloClient } from "@apollo/client";
-import { Button, Textarea, TextInput } from "@mantine/core";
+import { Button, Stack, Textarea, TextInput } from "@mantine/core";
 import { hasLength, isNotEmpty } from "@mantine/form";
 import { usePromiseStatusWithToast } from "@/core/utils/promise";
 import { ensureFetchResultData } from "@/core/utils/apollo";
@@ -70,55 +70,56 @@ export default function FieldCreateForm(props: FieldCreateFormProps) {
 
   return (
     <FormProvider form={form}>
-      <TextInput
-        key={form.key("name")}
-        {...form.getInputProps("name")}
-        label={t("labels.name")}
-        placeholder={t("labels.namePlaceholder")}
-        required
-      />
-      <AddressPicker
-        key={form.key("address")}
-        value={form.values.address}
-        onChange={(address, details) => {
-          form.setFieldValue("address", address);
-          if (!address) {
-            form.setFieldValue("latitude", null);
-            form.setFieldValue("longitude", null);
-            return;
+      <Stack>
+        <TextInput
+          key={form.key("name")}
+          {...form.getInputProps("name")}
+          label={t("labels.name")}
+          placeholder={t("labels.namePlaceholder")}
+          required
+        />
+        <AddressPicker
+          key={form.key("address")}
+          value={form.values.address}
+          onChange={(address, details) => {
+            form.setFieldValue("address", address);
+            if (!address) {
+              form.setFieldValue("latitude", null);
+              form.setFieldValue("longitude", null);
+              return;
+            }
+            assert(details && details.location);
+            form.setFieldValue("latitude", details.location.lat());
+            form.setFieldValue("longitude", details.location.lng());
+          }}
+          placeDetailsFields={["location"]}
+          error={
+            form.getInputProps("address").error ||
+            form.getInputProps("latitude").error ||
+            form.getInputProps("longitude").error
           }
-          assert(details && details.location);
-          form.setFieldValue("latitude", details.location.lat());
-          form.setFieldValue("longitude", details.location.lng());
-        }}
-        placeDetailsFields={["location"]}
-        error={
-          form.getInputProps("address").error ||
-          form.getInputProps("latitude").error ||
-          form.getInputProps("longitude").error
-        }
-        label={t("labels.address")}
-        placeholder={t("labels.addressPlaceholder")}
-        mt={10}
-      />
-      <Textarea
-        key={form.key("description")}
-        {...form.getInputProps("description")}
-        label={t("labels.description")}
-        placeholder={t("labels.descriptionPlaceholder")}
-      />
-      <TextInput
-        key={form.key("publicURL")}
-        {...form.getInputProps("publicURL")}
-        label={t("labels.publicURL")}
-        placeholder={t("labels.publicURLPlaceholder")}
-      />
+          label={t("labels.address")}
+          placeholder={t("labels.addressPlaceholder")}
+        />
+        <Textarea
+          key={form.key("description")}
+          {...form.getInputProps("description")}
+          label={t("labels.description")}
+          placeholder={t("labels.descriptionPlaceholder")}
+        />
+        <TextInput
+          key={form.key("publicURL")}
+          {...form.getInputProps("publicURL")}
+          label={t("labels.publicURL")}
+          placeholder={t("labels.publicURLPlaceholder")}
+        />
+      </Stack>
       <Button
         onClick={() => form.onSubmit(handleSubmit)()}
         loading={createFieldStatus === "pending"}
         fullWidth
         size="sm"
-        mt={20}
+        mt="xl"
       >
         {t("submit")}
       </Button>
