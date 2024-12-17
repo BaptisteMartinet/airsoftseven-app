ca4cc56e2e645630827f84b8cdab5cb264a598af
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-12-17 21:43:07 +0100

    feat: rework ResourceBanner with img

--- src/components/common/ResourceBanner.tsx
+++ src/components/common/ResourceBanner.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from "react";
 
-import { Box, Text, Title } from "@mantine/core";
+import { AspectRatio, Box, Group, Image, Overlay, Text, Title } from "@mantine/core";
 import { useMobile } from '@/core/utils/hooks';
 
 export interface ResourceBannerProps {
@@ -8,32 +8,37 @@ export interface ResourceBannerProps {
   title: string;
   subtitle?: string;
   actions: ReactNode;
-  bgColor: string;
+  imgSrc: string;
+  imgAlt?: string;
 }
 
 export default function ResourceBanner(props: ResourceBannerProps) {
-  const { resourceName, title, subtitle, actions, bgColor } = props;
+  const { resourceName, title, subtitle, actions, imgSrc, imgAlt } = props;
   const mobile = useMobile();
 
   return (
-    <Box
-      pos="relative"
-      h={400}
-      bg={bgColor}
-      c="white"
-      display="flex"
-      style={{ alignItems: "center", justifyContent: "center" }}
-    >
-      <Box>
-        <Text fw={500}>{resourceName}</Text>
-        <Title size={mobile ? 22 : 70} lineClamp={1}>
-          {title}
-        </Title>
-        {subtitle ? <Text ta="right">{subtitle}</Text> : null}
-      </Box>
-      <Box pos="absolute" bottom={20} right={20}>
-        {actions}
-      </Box>
-    </Box>
+    <AspectRatio ratio={16 / 3} pos="relative">
+      <Image src={imgSrc} height="100%" alt={imgAlt} />
+      <Overlay backgroundOpacity={0.3} blur={10} zIndex="auto">
+        <Box
+          pos="relative"
+          h="100%"
+          c="white"
+          display="flex"
+          style={{ alignItems: "center", justifyContent: "center" }}
+        >
+          <Box>
+            <Text fw={500}>{resourceName}</Text>
+            <Title size={mobile ? 22 : 70} lineClamp={1}>
+              {title}
+            </Title>
+            {subtitle ? <Text ta="right">{subtitle}</Text> : null}
+          </Box>
+          <Box pos="absolute" bottom={20} right={20}>
+            {actions}
+          </Box>
+        </Box>
+      </Overlay>
+    </AspectRatio>
   );
 }
