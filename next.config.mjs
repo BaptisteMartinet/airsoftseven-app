695f77a0c9c4a9d7339d4ff57f92cfae4ddaac50
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-12-18 23:51:02 +0100

    feat: adjust next config http redirect

--- next.config.mjs
+++ next.config.mjs
@@ -7,13 +7,16 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
-  redirects: async () => {
+  redirects: () => {
     return [
       {
         source: '/:path*',
         has: [
-          { type: 'header', key: 'host', value: 'www.airsoftseven.com' },
-          { type: 'header', key: 'host', value: 'airsoftseven.com' }
+          {
+            type: 'header',
+            key: 'x-forwarded-proto',
+            value: 'http',
+          },
         ],
         destination: 'https://airsoftseven.com/:path*',
         permanent: true,
